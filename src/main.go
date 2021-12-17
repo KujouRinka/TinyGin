5543ba89793b13f24edc7c33693a9986af3ae269
KujouRinka <kujourinka@gmail.com>
2021-12-17 19:31:06 +0800

    first step of tiny_gin

--- src/main.go
+++ src/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"TinyGin/src/tiny_gin"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	r := tiny_gin.New()
+	r.GET("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	})
+	r.GET("/hello", func(w http.ResponseWriter, r *http.Request) {
+		for k, v := range r.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
+
+	r.Run(":8888")
 }
